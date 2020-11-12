412219d37faf01fd2912cf42f8da3d89beeab4b2
Nazaré da Piedade <nazarepiedady@gmail.com>
2020-11-12 13:42:04 +0100

    Create activeSlide method
    Get a specific index, set active property to index,
    set a nodeList to the items property and select the
    item in the index position then add a active class
    to it.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 class SlideStories {
   constructor(id) {
     this.$slide = document.querySelector(`[data-slide="${id}"]`);
+    this.current = 0;
+  }
+
+  activeSlide(index) {
+    this.active = index;
+    this.items = this.$slide.querySelectorAll('.slide__item');
+    this.items[index].classList.add('slide__item--active');
   }
 }
 
